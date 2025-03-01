564e81fd8a8ea9714bd7306bc3c14fd0ef2f352f
arunsh52 <arunsharma13810@gmail.com>
2025-03-01 22:30:13 +0530

    add education

--- main.js
+++ main.js
@@ -34,4 +34,16 @@ navLinks.forEach(link => {
 })
 
 
+const scrollToTopButton = document.querySelector('.scroll-to-top');
 
+// Function to toggle the visibility of the scroll-to-top button
+function toggleScrollToTopButton() {
+  if (window.scrollY > 100) { // When the user has scrolled down 100px or more
+    scrollToTopButton.classList.add('show');
+  } else {
+    scrollToTopButton.classList.remove('show');
+  }
+}
+
+// Listen for scroll events
+window.addEventListener('scroll', toggleScrollToTopButton);
